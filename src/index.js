7c30e8079e98bdd8639a30d8d13d0d8d50d20e28
Peter-PM <tangompp@gmail.com>
2021-09-24 00:18:54 +0300

    del redux & inline style

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/app/app';
 
-document.getElementById('root').style = "display: flex; flex-direction: column; justify-content: space-between; min-height: 100vh;";
-
 ReactDOM.render(
   <React.StrictMode>
     <App/>
